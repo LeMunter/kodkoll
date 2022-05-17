bd9dd0d867fa53690098e05da81dfb4ad6f22c06
lemunter <antonmunter@gmail.com>
2022-05-17 20:25:16 +0200

    Add folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "hero-img": "url('/bg.jpg')",
+      },
       colors: {},
     },
   },
